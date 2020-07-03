93e66086a32070d7cd1bf34f3c092af212413257
Oladipo George <admin@imarveldesign.co.uk>
2020-07-03 17:33:32 +0100

    Scan functionality.

--- lib/services/push_notification_service.dart
+++ lib/services/push_notification_service.dart
@@ -2,11 +2,19 @@ import 'dart:io';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bonapp/models/user.dart';
+import 'package:flutter_bonapp/services/user_service.dart';
 import 'package:flutter_bonapp/utils/constants.dart';
 import 'package:flutter_bonapp/utils/routing_constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'locator.dart';
 
 class PushNotificationService {
+  UserService userService = locator<UserService>();
   final FirebaseMessaging _fcm = FirebaseMessaging();
+
+  User user;
 
   Future initialise(BuildContext context) async {
     if (Platform.isIOS) {
@@ -33,7 +41,7 @@ class PushNotificationService {
     );
   }
 
-  void _serialiseAndNavigate(Map<String, dynamic> message, BuildContext context) {
+  Future<void> _serialiseAndNavigate(Map<String, dynamic> message, BuildContext context) async {
     var notificationData = message['data'];
     var view = notificationData['view'];
 
@@ -41,6 +49,13 @@ class PushNotificationService {
       // Navigate to the ---
       if (view == 'new_message') {
         Navigator.pushNamed(context, MessagesScreenRoute);
+      }
+
+      if (view == 'new_voucher') {
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        int uid = prefs.getInt('userId');
+        user = await userService.getUser(uid);
+        Navigator.pushNamed(context, InitialScreenRoute, arguments: user);
       }
     }
   }
